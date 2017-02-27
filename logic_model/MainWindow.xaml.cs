c3c4c2da8ac3091421cbdccd0ca4a8f996f6721e
Schalaschaska <mihanhlystow@gmail.com>
2017-02-27 23:51:32 +0300

    Com 2

--- logic_model/MainWindow.xaml.cs
+++ logic_model/MainWindow.xaml.cs
@@ -26,40 +26,47 @@ namespace logic_model
         }
         
         public string rezult;
-       
-       
 
+
+        public Button yes = new Button();
+        public Button no = new Button();
+        public bool flag=false;
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            button.Visibility = Visibility.Hidden;
-            label.Content = "Это животное летает?";
-            Button yes = new Button();
-            yes.Width = 97;
-            yes.Height = 35;
-            yes.Content = "Да";
-            yes.HorizontalAlignment = HorizontalAlignment.Left;
-            yes.VerticalAlignment = VerticalAlignment.Top;
-            yes.Margin = new Thickness(10, 151, 0, 0);
-            Big_grid.Children.Add(yes);
-            yes.Click += new RoutedEventHandler(yes_click);
-            Button no = new Button();
-            no.Width = 97;
-            no.Height = 35;
-            no.Content = "Нет";
-            no.HorizontalAlignment = HorizontalAlignment.Left;
-            no.VerticalAlignment = VerticalAlignment.Top;
-            no.Margin = new Thickness(178, 151, 0, 0);
-            Big_grid.Children.Add(no);
-            no.Click += new RoutedEventHandler(no_click);
+                
+                button.Visibility = Visibility.Hidden;
+                label.Content = "Это животное летает?";
+                yes.Width = 97;
+                yes.Height = 35;
+                yes.Content = "Да";
+                yes.HorizontalAlignment = HorizontalAlignment.Left;
+                yes.VerticalAlignment = VerticalAlignment.Top;
+                yes.Margin = new Thickness(10, 151, 0, 0);
+                Big_grid.Children.Add(yes);
+                yes.Click += new RoutedEventHandler(yes_click);
+                no.Width = 97;
+                no.Height = 35;
+                no.Content = "Нет";
+                no.HorizontalAlignment = HorizontalAlignment.Left;
+                no.VerticalAlignment = VerticalAlignment.Top;
+                no.Margin = new Thickness(178, 151, 0, 0);
+                Big_grid.Children.Add(no);
+                no.Click += new RoutedEventHandler(no_click);
+                if(flag==true)
+            {
+                this.Close();
+                
 
+            }
 
         }
         private void yes_click(object sender, RoutedEventArgs e)
         {
+            flag = true;
             rezult += "1";
             if (rezult == "1") 
             {
-                label.Content = "Яркая окраска&";
+                label.Content = "Яркая окраска?";
             }
             if(rezult=="11")
             {
@@ -68,31 +75,73 @@ namespace logic_model
             if(rezult=="111")
             {
                 label.Content = "Это попугай!";
+                yes.Visibility = Visibility.Hidden;
+                no.Visibility = Visibility.Hidden;
+                Big_grid.Children.Remove(yes);
+                Big_grid.Children.Remove(no);
+                button.Content = "Выход";
+                button.Visibility = Visibility.Visible;
+                flag = true;
+                              
+                
+
             }
             if(rezult=="101")
             {
                 label.Content = "Это пеликан";
+                yes.Visibility = Visibility.Hidden;
+                no.Visibility = Visibility.Hidden;
+                Big_grid.Children.Remove(yes);
+                Big_grid.Children.Remove(no);
+                button.Content = "Выход";
+                button.Visibility = Visibility.Visible;
+                flag = true;
+
             }
-            if(rezult=="001")
+            if (rezult=="001")
             {
                 label.Content = "Имееи ли иголки?";
             }
             if(rezult=="0011")
             {
                 label.Content = "Это йожик";
+                yes.Visibility = Visibility.Hidden;
+                no.Visibility = Visibility.Hidden;
+                Big_grid.Children.Remove(yes);
+                Big_grid.Children.Remove(no);
+                button.Content = "Выход";
+                button.Visibility = Visibility.Visible;
+                flag = true;
+
             }
-        
-            if(rezult=="01")
+
+            if (rezult=="01")
             {
                 label.Content = "Гавкает?";
             }
             if(rezult=="011")
             {
                 label.Content = "Это собака";
+                yes.Visibility = Visibility.Hidden;
+                no.Visibility = Visibility.Hidden;
+                Big_grid.Children.Remove(yes);
+                Big_grid.Children.Remove(no);
+                button.Content = "Выход";
+                button.Visibility = Visibility.Visible;
+                flag = true;
+
             }
             if (rezult == "0001")
             {
                 label.Content = "Это единорог!";
+                yes.Visibility = Visibility.Hidden;
+                no.Visibility = Visibility.Hidden;
+                Big_grid.Children.Remove(yes);
+                Big_grid.Children.Remove(no);
+                button.Content = "Выход";
+                button.Visibility = Visibility.Visible;
+                flag = true;
+
             }
         }
             
@@ -111,6 +160,13 @@ namespace logic_model
             if(rezult=="100")
             {
                 label.Content = "Это воробей";
+                yes.Visibility = Visibility.Hidden;
+                no.Visibility = Visibility.Hidden;
+                Big_grid.Children.Remove(yes);
+                Big_grid.Children.Remove(no);
+                button.Content = "Выход";
+                button.Visibility = Visibility.Visible;
+                flag = true;
             }
             if(rezult=="00")
             {
@@ -119,10 +175,24 @@ namespace logic_model
             if(rezult=="0010")
             {
                 label.Content = "Это мышь";
+                yes.Visibility = Visibility.Hidden;
+                no.Visibility = Visibility.Hidden;
+                Big_grid.Children.Remove(yes);
+                Big_grid.Children.Remove(no);
+                button.Content = "Выход";
+                button.Visibility = Visibility.Visible;
+                flag = true;
             }
             if(rezult=="010")
             {
                 label.Content = "Это кошка";
+                yes.Visibility = Visibility.Hidden;
+                no.Visibility = Visibility.Hidden;
+                Big_grid.Children.Remove(yes);
+                Big_grid.Children.Remove(no);
+                button.Content = "Выход";
+                button.Visibility = Visibility.Visible;
+                flag = true;
             }
             if (rezult == "000")
             {
@@ -131,11 +201,26 @@ namespace logic_model
             if(rezult=="0000")
             {
                 label.Content = "Это тирекс!";
+                yes.Visibility = Visibility.Hidden;
+                no.Visibility = Visibility.Hidden;
+                Big_grid.Children.Remove(yes);
+                Big_grid.Children.Remove(no);
+                button.Content = "Выход";
+                button.Visibility = Visibility.Visible;
+                flag = true;
             }
           
             if(rezult=="110")
             {
                 label.Content = "Это райская птица!";
+                yes.Visibility = Visibility.Hidden;
+                no.Visibility = Visibility.Hidden;
+                Big_grid.Children.Remove(yes);
+                Big_grid.Children.Remove(no);
+                button.Content = "Выход";
+                button.Visibility = Visibility.Visible;
+                flag = true;
+
             }
 
             }
